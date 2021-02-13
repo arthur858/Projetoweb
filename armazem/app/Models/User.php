18fcd647ad40ea96f9c32e271f37a027fd674399
arthur858 <josetrevisan04@gmail.com>
2021-02-13 02:58:58 -0300

    adicionando relacionamento no model user

--- armazem/app/Models/User.php
+++ armazem/app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function produto(){
+
+        return $this->hasMany(Produto::class);
+    }
 }
