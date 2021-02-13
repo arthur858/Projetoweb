da4587a1cac4350e08f79c05b9e7e648df1390f1
arthur858 <josetrevisan04@gmail.com>
2021-02-13 02:51:54 -0300

    Adicionando models e migrations para o banco de dados

--- armazem/database/migrations/2021_02_13_054413_create_produtos_table.php
+++ armazem/database/migrations/2021_02_13_054413_create_produtos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProdutosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->integer('codigo');
             $table->string('name');
-            $table->string('cpf');
-            $table->string('data_de_nascimento');
-            $table->string('tipo');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('marca');
+            $table->string('data_de_validade');
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('produtos');
     }
 }
